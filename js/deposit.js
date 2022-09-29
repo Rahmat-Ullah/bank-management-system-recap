1bcf1a75ca3e41a7c00b4afeabe17cf2c8a8d321
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-29 07:09:13 +0600

    deposit section interactived

---
+++ js/deposit.js
@@ -0,0 +1,21 @@
+// step 1: add event listener to deposit button
+
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    //step 2: get deposit amount from deposit field
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    //step 3: convert string deposit amount to a number type
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    //step 4: clear the deposit input
+    depositField.value = ('');
+
+    //step 5: get the previous deposit total
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    //step 6: calculate new deposit total and set the value to the deposit total
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = newDepositTotal;
+})
