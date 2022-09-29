8c21afbea3d9f3bf65385214d884acd78bf0f9f2
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-29 07:28:44 +0600

    deposit section practiced again

--- js/deposit.js
+++ js/deposit.js
@@ -4,18 +4,29 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     //step 2: get deposit amount from deposit field
     const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
+
+    // const depositField = document.getElementById('deposit-field');
+    // const newDepositAmountString = depositField.value;
     //step 3: convert string deposit amount to a number type
     const newDepositAmount = parseFloat(newDepositAmountString);
+    // const newDepositAmount = parseFloat(newDepositAmountString);
 
     //step 4: clear the deposit input
     depositField.value = ('');
+    // depositField.value = ('');
 
     //step 5: get the previous deposit total
     const depositTotalElement = document.getElementById('deposit-total');
     const previousDepositTotalString = depositTotalElement.innerText;
     const previousDepositTotal = parseFloat(previousDepositTotalString);
+    // const depositTotalElement = document.getElementById('deposit-total');
+    // const previousDepositTotalString = depositTotalElement.innerText;
+    // const previousDepositTotal = parseFloat(previousDepositTotalString);
 
     //step 6: calculate new deposit total and set the value to the deposit total
     const newDepositTotal = previousDepositTotal + newDepositAmount;
     depositTotalElement.innerText = newDepositTotal;
+
+    // const newDepositTotal = previousDepositTotal + newDepositAmount;
+    // depositTotalElement.innerText = newDepositTotal;
 })
